e3e0c4a00e216b3ccd64ad0fc8ab9d214fcdd4f2
vikstasenkova <viktoria_stasenkova@mail.ru>
2022-05-21 01:50:06 +0300

    add initialScreen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Header from './components/header'
 import { Routes, Route } from "react-router-dom";
+import InitialScreen from "./pages/Initial"
 
 function App() {
   return (
     <div className='w-screen h-screen bg-bg-grey'>
       <Header />
       <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} /> */}
+        <Route path="/" element={<InitialScreen />} />
       </Routes>
     </div>
   );
--- src/pages/Initial/index.js
+++ src/pages/Initial/index.js
@@ -1,12 +1,15 @@
-import { Images } from "../Images";
-import Input from "../Input";
+import { Images } from "../../components/Images";
 
 
-function Header() {
+
+function InitialScreen() {
     return (
-        <div className="h-[72px] w-full bg-blue">
-
+        <div className="w-full h-[calc(100%-72px)] flex  justify-center items-center overflow-hidden">
+            <div className="flex flex-col items-center gap-[47px]">
+                <Images type={'search-logo'} className={"w-[65px] h-[65px]"} />
+                <p className="w-[210px] text-center text-grey-1 leading-[30.8px] text-[22px] font-normal ">Start with searching a GitHub user</p>
+            </div>
         </div>
     );
 }
-export default Header;
+export default InitialScreen;
